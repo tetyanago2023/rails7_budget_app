b772b24a1489d300824de8cc995d34c1658c469b
go2024 <tetyanago2018@gmail.com>
2024-01-08 16:53:14 -0800

    categories colors rendering adjustment

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,13 +1,15 @@
 module CategoriesHelper
   def tailwind_color(category)
     color_variants = {
-      rose: 'bg-rose-400 hover:bg-rose-400',
-      light_blue: 'bg-sky-400 hover:bg-sky-400',
+      rose: 'bg-rose-400',
+      light_blue: 'bg-sky-400',
+      lime: 'bg-lime-400',
+      light_gray: 'bg-gray-400'
     }
     if category.present? && category.color.present?
       color_variants[category.color.to_sym]
     else
-      'bg-gray-100 hover:bg-gray-100'
+      'bg-gray-100'
     end
   end
 end
